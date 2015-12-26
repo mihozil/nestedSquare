37b8914380cc47e3219a4b39fa1bdbe6eae56554
Apple <Apple@Apples-MacBook-Air.local>
2015-12-26 15:52:22 +0700

    Initial Commit

---
+++ nestedSquare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nestedSquare
+//
+//  Created by Apple on 12/26/15.
+//  Copyright (c) 2015 AMOSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nestedSquare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nestedSquare
+//
+//  Created by Apple on 12/26/15.
+//  Copyright (c) 2015 AMOSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
